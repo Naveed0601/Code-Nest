aff9c486ca22b7c4f1dac8e5ddc5129de3c7abc4
wireframeee <brightly2469@gmail.com>
2025-03-05 23:43:52 +0530

    codeblocks implemented

--- frontend/src/component/WhiteBoard.js
+++ frontend/src/component/WhiteBoard.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { ReactFlow, Controls, Background } from "@xyflow/react";
+import { useState, useCallback } from 'react';
+import { ReactFlow, Controls, Background, applyNodeChanges} from "@xyflow/react";
+import { CodeBlocks } from './CodeBlocks';
 import '@xyflow/react/dist/style.css';
 
-
 export const WhiteBoard = () => {
+
+  const [nodes, setNodes] = useState(CodeBlocks);
+ 
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [],
+  );
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}> 
-      <ReactFlow style={{ width: '100%', height: '100%' }}>
+      <ReactFlow nodes={nodes} onNodesChange={onNodesChange} style={{ width: '100%', height: '100%' }}>
         <Background />
         <Controls />
       </ReactFlow>
